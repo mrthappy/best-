3aa479dd6bbb56a3c6820c1fa0a706d7bffe71e6
mrthappy <mrthappymailz@gmail.com>
2018-12-27 17:13:23 +0100

    kdkd

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
-gffg
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Project</title>
+     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,400" rel="stylesheet">
+
+    <!--  <link rel="stylesheet"  type="text/css"  href="../stylesheet/newcss.css">-->
+        <link rel="stylesheet"  type="text/css"  href="../stylesheet/newcss.css">
+
+     <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <script src="../javascript/app.js"></script>
+    <!-- <script src="../javascript/java.js"></script>-->
+     <!--<script src= "https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>-->
+      <!--<script src="https://code.angularjs.org/1.6.9/angular-route.js"></script>-->
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <style>
+
+  </style>
+</head>
