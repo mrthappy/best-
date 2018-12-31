33a63acf9be60fefc8c6eab6c7d214da29d835fa
mrthappy <mrthappymailz@gmail.com>
2018-12-31 15:07:04 +0100

    add form

--- public/index.php
+++ public/index.php
@@ -68,9 +68,28 @@ include "../database/database.php";
 
   <section class="contactform">
     <form method="POST" action="">
-<h1>jjsj</h1>
-    </input>
-  </form>
+      <form class="login_form"  name="loginForm" action="test.php" METHOD="POST" ng-controller="controller">
+
+          <div class="div">
+              <i class="far fa-envelope"></i>
+              <input type="email" name="email"  id="email" Placeholder="E-mail" class="input">
+
+          </div>
+
+          <div class="div">
+              <i class="fas fa-unlock"></i>
+              <input type="password" name="password" id="password" placeholder="Password" class="input">
+
+          </div>
+
+          <div class="div">
+              <input type="submit" value="LOGIN" class="submitbtn"name="submit">
+          </div>
+          <span class="register_span">Do You have an account?<a href="login.php?login=register"class="register">Register</a></span>
+          <span class="password_reset"><a href="login.php?login=password" class="register">Forgot Password</a></span>
+
+
+      </form>
   </section>
   <section class="footerdiv">
 
